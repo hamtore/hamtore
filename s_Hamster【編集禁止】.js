4ce0351b4817d050ccc42c02fac83123b737c5f1
colticol <34980690+colticol@users.noreply.github.com>
2021-01-03 20:35:37 +0900

    fix cancel

--- s_Hamster【編集禁止】.js
+++ s_Hamster【編集禁止】.js
@@ -156,8 +156,9 @@ function createOrder_(strategy, position, leverage, memo, position_size, limitpr
     message = "===================\nPosition:" + position + "\nStrategy:" + strategy + "\nMemo:" + memo + "\nExchange:" + exchange;
     sendMessage_(message);
     return Status.success;
-  }else if(position == "CANCEL"){
-    cancelLimitOrder_(productcode,position,strategy,exchange);
+  }else if(position == "BUYCANCEL" || position == "SELLCANCEL"){
+    // exclude suffix "CANCEL"
+    cancelLimitOrder_(productcode, position.substring(0, position.length-6), strategy, exchange);
     return Status.success;
   }else if(active.toUpperCase() == "ON"){
     var time, price, collateral, lastTradeInfo, message, tid;
